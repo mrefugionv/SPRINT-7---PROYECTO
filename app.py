9488e8921f5669ea24bf13efd8a4b616a2b4eb7e
mrefugionv <mrefugionoriegav@gmail.com>
2025-02-14 18:09:34 -0600

    add app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Proyecto Sprint 7')
+
+car_data = pd.read_csv('C:/Users/cucum/OneDrive/Documentos/.PY/SPRINT-7---PROYECTO/vehicles_us.csv')  # leer los datos
+
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+#scatter_button = st.button('Construir gráfico de dispersión')  # crear segundo botón
+scatter_checkbox = st.checkbox('Construir gráfico de dispersión') # Checkbox en lugar de boton
+
+if scatter_checkbox:  # al verificar casilla
+    # escribir un mensaje
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig2 = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
